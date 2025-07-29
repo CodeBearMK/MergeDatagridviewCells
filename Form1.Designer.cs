9750147db75bf3e127a7e62de84f84faa7c93ea7
鄭麥克 <cat32tw@hotmail.com>
2025-07-30 01:05:19 +0800

    2025.0730.01.01-Datagridview合併儲存格

---
+++ Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace MergeDatagridviewCells
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgv = new System.Windows.Forms.DataGridView();
+            this.ProduceModel = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ProducePart = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ProduceSpec = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgv
+            // 
+            this.dgv.AllowUserToAddRows = false;
+            this.dgv.AllowUserToDeleteRows = false;
+            this.dgv.BackgroundColor = System.Drawing.Color.White;
+            this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ProduceModel,
+            this.ProducePart,
+            this.ProduceSpec});
+            this.dgv.Location = new System.Drawing.Point(12, 77);
+            this.dgv.Name = "dgv";
+            this.dgv.ReadOnly = true;
+            this.dgv.RowHeadersWidth = 51;
+            this.dgv.RowTemplate.Height = 27;
+            this.dgv.Size = new System.Drawing.Size(713, 342);
+            this.dgv.TabIndex = 0;
+            this.dgv.CellPainting += new System.Windows.Forms.DataGridViewCellPaintingEventHandler(this.dgv_CellPainting);
+            // 
+            // ProduceModel
+            // 
+            this.ProduceModel.HeaderText = "產品類";
+            this.ProduceModel.MinimumWidth = 6;
+            this.ProduceModel.Name = "ProduceModel";
+            this.ProduceModel.ReadOnly = true;
+            this.ProduceModel.Width = 125;
+            // 
+            // ProducePart
+            // 
+            this.ProducePart.HeaderText = "產品料號";
+            this.ProducePart.MinimumWidth = 6;
+            this.ProducePart.Name = "ProducePart";
+            this.ProducePart.ReadOnly = true;
+            this.ProducePart.Width = 125;
+            // 
+            // ProduceSpec
+            // 
+            this.ProduceSpec.HeaderText = "產品規格";
+            this.ProduceSpec.MinimumWidth = 6;
+            this.ProduceSpec.Name = "ProduceSpec";
+            this.ProduceSpec.ReadOnly = true;
+            this.ProduceSpec.Width = 125;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(737, 487);
+            this.Controls.Add(this.dgv);
+            this.Name = "Form1";
+            this.Text = "合併 Datagridview 儲存格";
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgv;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ProduceModel;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ProducePart;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ProduceSpec;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,143 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace MergeDatagridviewCells
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            LoadData();
+            //MergeCells();
+        }
+
+        void LoadData()
+        {
+            List<ProduceData> produceDatas = new List<ProduceData>();
+            produceDatas.Add(new ProduceData { ProduceModel = "AA", ProducePart = "AAS10", ProduceSpec = "TON1" });
+            produceDatas.Add(new ProduceData { ProduceModel = "AA", ProducePart = "AAS10", ProduceSpec = "TON1" });
+            produceDatas.Add(new ProduceData { ProduceModel = "AA", ProducePart = "AAS10", ProduceSpec = "TON1" });
+            produceDatas.Add(new ProduceData { ProduceModel = "AA", ProducePart = "AAS10", ProduceSpec = "TON1" });
+            produceDatas.Add(new ProduceData { ProduceModel = "AA", ProducePart = "AAS10", ProduceSpec = "TON1" });
+            produceDatas.Add(new ProduceData { ProduceModel = "AB", ProducePart = "ABS10", ProduceSpec = "TON2" });
+            produceDatas.Add(new ProduceData { ProduceModel = "AB", ProducePart = "ABS10", ProduceSpec = "TON2" });
+            produceDatas.Add(new ProduceData { ProduceModel = "AB", ProducePart = "ABS10", ProduceSpec = "TON2" });
+            produceDatas.Add(new ProduceData { ProduceModel = "AB", ProducePart = "ABS10", ProduceSpec = "TON2" });
+            dgv.RowCount = produceDatas.Count();
+            for (int i = 0; i < produceDatas.Count(); i++)
+            {
+                dgv.Rows[i].Cells[0].Value = produceDatas[i].ProduceModel;
+                dgv.Rows[i].Cells[1].Value = produceDatas[i].ProducePart;
+                dgv.Rows[i].Cells[2].Value = produceDatas[i].ProduceSpec;
+            }
+
+        }
+
+        private void dgv_CellPainting(object sender, DataGridViewCellPaintingEventArgs e)
+        {
+            // 只對第0欄進行處理（可自行調整）
+            if ((e.ColumnIndex == 0 || e.ColumnIndex == 1 || e.ColumnIndex == 2) && e.RowIndex >= 0)
+            {
+                DataGridView dgv = sender as DataGridView;
+                string currValue = e.Value?.ToString();
+                if (string.IsNullOrEmpty(currValue)) return;
+
+                // 是否是第一列或與前一列不同
+                bool isFirstRow = e.RowIndex == 0 ||
+                    dgv.Rows[e.RowIndex - 1].Cells[e.ColumnIndex].Value?.ToString() != currValue;
+
+                if (isFirstRow)
+                {
+                    int rowSpan = 1;
+                    int totalHeight = dgv.Rows[e.RowIndex].Height;
+
+                    // 計算有幾列要合併
+                    for (int i = e.RowIndex + 1; i < dgv.Rows.Count; i++)
+                    {
+                        string nextValue = dgv.Rows[i].Cells[e.ColumnIndex].Value?.ToString();
+                        if (nextValue != currValue)
+                            break;
+
+                        rowSpan++;
+                        totalHeight += dgv.Rows[i].Height;
+                    }
+
+                    // 畫背景
+                    e.Graphics.FillRectangle(new SolidBrush(e.CellStyle.BackColor),
+                        new Rectangle(e.CellBounds.X, e.CellBounds.Y, e.CellBounds.Width, totalHeight));
+
+                    // 畫邊框
+                    using (Pen gridLinePen = new Pen(dgv.GridColor))
+                    {
+                        e.Graphics.DrawRectangle(gridLinePen,
+                            new Rectangle(e.CellBounds.X, e.CellBounds.Y, e.CellBounds.Width - 1, totalHeight - 1));
+                    }
+
+                    // 畫文字（垂直 + 水平置中）
+                    TextFormatFlags flags = TextFormatFlags.VerticalCenter | TextFormatFlags.HorizontalCenter | TextFormatFlags.WordBreak;
+                    Rectangle mergedRect = new Rectangle(e.CellBounds.X, e.CellBounds.Y, e.CellBounds.Width, totalHeight);
+                    TextRenderer.DrawText(e.Graphics, currValue, e.CellStyle.Font, mergedRect, e.CellStyle.ForeColor, flags);
+
+                    e.Handled = true;
+                }
+                else
+                {
+                    // 非起始列不繪製任何內容，讓它顯示合併視覺效果
+                    e.Handled = true;
+                }
+            }
+        }
+
+        //private void MergeCells()
+        //    {
+        //        int RowCount = dgv.Rows.Count;
+        //        int ColCount = dgv.Columns.Count;
+        //        int startIdx = 0;
+        //        int endIdx = 0;
+        //        object currVal = null;
+        //        object nextVal = null;
+        //        if (RowCount > 0)
+        //        {
+        //            for (int c = 0; c < ColCount; c++)
+        //            {
+        //                for (int r = 0 ; r < RowCount; r++)
+        //                {
+        //                    if (r != RowCount - 1)
+        //                    {
+        //                        currVal = dgv[c, r].Value;
+        //                        nextVal = dgv[c, r + 1].Value;
+        //                        if (currVal == nextVal)
+        //                        {
+        //                            dgv[c, r].Value = null;
+        //                            dgv.AdvancedCellBorderStyle.Left = DataGridViewAdvancedCellBorderStyle.Single;
+        //                            dgv.AdvancedCellBorderStyle.Right = DataGridViewAdvancedCellBorderStyle.Single;
+        //                            endIdx++;
+        //                            continue;
+        //                        }
+        //                    }
+        //                    int midIdx = ((endIdx - startIdx) + 1) / 2;
+        //                    midIdx += startIdx;
+        //                    dgv[c, midIdx].Value = dgv[c, endIdx].Value;
+        //                    dgv[c, r].Value = null;
+        //                    endIdx++;
+        //                    startIdx = endIdx;
+
+        //                }
+        //                startIdx = 0;
+        //                endIdx = 0;
+        //            }
+        //        }
+        //    }
+        //}
+
+        public class ProduceData
+        {
+            public string ProduceModel { get; set; }
+            public string ProducePart { get; set; }
+            public string ProduceSpec { get; set; }
+        }
+    }
+}
